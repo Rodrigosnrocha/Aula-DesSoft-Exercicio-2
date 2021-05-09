b89c1b9dcb4bccb964c4c44630777ab69df767b9
Rsngamer <irmaosrsn@gmail.com>
2021-05-08 21:52:27 -0300

    Criada função de sorteio de baralho

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Aula-DesSoft-Exercicio-2
+
+Esse programa é o jogo Paciência Acordeão(Solitaire Accordion), feito para o EP2 da aula de Design de Software. Para rodar o programa abra no terminal do Python
+
+Changelog:
+
+0.1.0 - Versão inicial. Criada função de sorteio de baralho. Atualmente imprime o baralho gerado por motivos de teste
---
+++ jogo.py
@@ -0,0 +1,13 @@
+import random
+#'J♠', 'A♥', '4♣', '7♦'
+
+
+def cria_baralho(): #Cria um baralho aleatório sem repetições
+    cards = ['A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦']
+    
+    random.shuffle(cards)
+    return cards
+
+
+baralho = cria_baralho()
+print(baralho)
